5535ead668c2245a8d3188e1471f7f47708aefa5
mohammed sohail <mohammedsohail422@gmail.com>
2022-05-15 12:13:42 +0530

    proj em

---
+++ email_validation/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send('hello from sohail')
+})
+app.listen(port, () => {
+  console.log('server is running at 3000');
+})
